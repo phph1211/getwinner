49dd9f1b639db5ff25715ceca4ecb43c8e058a6a
박한 <bakan@baghan-ui-MacBookPro.local>
2024-11-09 16:08:53 +0900

    add pokjuk

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./countDown.css";
+import pokjuk from "./pokjuk.mp4";
 
 const App: React.FC = () => {
   const [responseData, setResponseData] = useState<any>(null);
   const [isEnd, setIsEnd] = useState(false);
+  const [videoReady, setVideoReady] = useState(false); // 비디오 재생을 위한 상태 추가
 
   useEffect(() => {
     axios
@@ -38,7 +40,16 @@ const App: React.FC = () => {
 
     setTimeout(() => {
       setIsEnd(true);
-    }, 12500);
+
+      // 비디오의 z-index를 999로 변경
+      const video = document.querySelector("video");
+      if (video) {
+        video.style.zIndex = "999";
+      }
+
+      // 12.5초 후 비디오 재생
+      setVideoReady(true);
+    }, 12500); // 12초에서 12.5초로 변경
 
     const countdownInterval = setInterval(updateCountdown, 1100);
 
@@ -52,8 +63,30 @@ const App: React.FC = () => {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        position: "relative",
       }}
     >
+      <video
+        muted
+        loop
+        className="background-video"
+        style={{
+          position: "absolute",
+          top: "0",
+          left: "0",
+          width: "100%",
+          height: "100%",
+        }}
+        ref={(videoElement) => {
+          if (videoElement && videoReady) {
+            videoElement.play(); // 12.5초 후 비디오 재생
+          }
+        }}
+      >
+        <source src={pokjuk} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
       <div className="container">
         <span>10</span>
         <span>9</span>
@@ -74,6 +107,8 @@ const App: React.FC = () => {
             color: "white",
             display: isEnd ? "flex" : "none",
             gap: "10em",
+            position: "absolute", // 비디오 위로 겹치도록
+            zIndex: 1000, // 비디오 위로 올리기
           }}
         >
           {responseData.data.slice(0, 2).map((winner: any, index: number) => (
